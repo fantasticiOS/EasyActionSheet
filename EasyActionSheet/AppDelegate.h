1b22cdd26b0962b4110ebda4b57248bcdd0e8525
fantasticiOS <422538722@qq.com>
2016-07-08 10:25:58 +0800

    Initial Commit

---
+++ EasyActionSheet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyActionSheet
+//
+//  Created by odm2 on 16/7/8.
+//  Copyright © 2016年 odm2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyActionSheet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyActionSheet
+//
+//  Created by odm2 on 16/7/8.
+//  Copyright © 2016年 odm2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
